f07638051a2a085e948af877744ec7e299333908
Ohleh <oleg.kolischuk@gmail.com>
2022-06-01 01:05:35 +0300

    1 country-info

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import fetchCountries from './js/fetchCountries';
 
 const refs = {
   serchInput: document.querySelector('#search-box'),
-  countryList: document.querySelector('country-list'),
-  countryInfo: document.querySelector('country-info'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
 const DEBOUNCE_DELAY = 300;
 
-refs.serchInput.addEventListener('input', onSearchIpnut);
+// refs.serchInput.addEventListener('input', onSearchIpnut);
 
-function onSearchIpnut(e) {
-  const searchInput = e.currentTarget.value;
-  fetchCountries(searchInput).then(response => console.log(response));
-}
+// function onSearchIpnut(e) {
+//   const searchInput = e.currentTarget.value;
+//   fetchCountries(searchInput).then(response => renderUserList(response));
+// }
 
 // const ddd = fetchCountries();
 // const aaa = ddd.map(e => e.name);
@@ -23,3 +23,25 @@ function onSearchIpnut(e) {
 // fetchCountries().then(response => {
 //   console.log(response[0].name.official);
 // });
+
+fetchCountries('japan').then(response => {
+  renderUserList(response);
+  // console.log(response);
+});
+//   .then(console.log(renderUserList));
+
+function renderUserList(name) {
+  const markup = name
+    .map(name => {
+      console.dir(name.flags);
+      const languaGua = name.languages;
+      return `<li>
+            <p><b>🇺🇦</b>: ${name.name.official}</p>
+            <p><b>Capital</b>: ${name.capital}</p>
+            <p><b>Population</b>: ${name.population}</p>
+            <p><b>Languages</b>: ${Object.values(languaGua)}</p>
+          </li>`;
+    })
+    .join('');
+  refs.countryInfo.innerHTML = markup;
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,8 @@
-export default function (name) {
+export default function fetchCountries(name) {
   return fetch(
     // `https://restcountries.com/v3.1/all?fields=name,capital,population,flags.svg,languages`
-    `https://restcountries.com/v2/all?fields=${name},capital,population,flags.svg,languages`
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    // `https://restcountries.com/v3.1/name/${name}`
   ).then(response => {
     return response.json();
   });
